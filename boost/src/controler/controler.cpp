511e0212bf3fbf03fb6cb91ca6980794e16a2a16
EvOr <villemin@poste.isima.fr>
2009-02-07 18:18:44 +0000

    Ajout gestion des stubs

--- boost/src/controler/controler.cpp
+++ boost/src/controler/controler.cpp
@@ -5,14 +5,14 @@ void Controler::change_file_name(std::string name){
    filename=name;
    graph.clear();
    mescoords.clear();
-  // position.clear();
+   // position.clear();
 }
 
 ///\brief parse le fichier
 void Controler::parse_file(){
-	std::cout << "parse file " << filename  << "..." << std::endl;
-        parse(filename, graph); 
-	std::cout << "num_vertices:" << boost::num_vertices(graph) << std::endl;
+   std::cout << "parse file " << filename  << "..." << std::endl;
+   parse(filename, graph); 
+   std::cout << "num_vertices:" << boost::num_vertices(graph) << std::endl;
 
 }
 
@@ -27,14 +27,28 @@ int Controler::displayCircle(double r)
    circle_graph_layout( graph, position, r);
    std::map<vertex_descriptor, double> v_centrality;
    tableau_de_poids vc_map(v_centrality);
-//   brandes_betweenness_centrality(graph, vc_map);
+   //   brandes_betweenness_centrality(graph, vc_map);
    return 0;
 
 }
 
+///\brief Parse le fichier de triplet afin d'affecter si oui ou non un AS(vertex) est un transit
+///\param filename nom du fichier de triplet
 void Controler::load_triplet(std::string const & filename){
    std::set<std::size_t> transit_as;
+   std::set<std::size_t>::iterator it;
+   Graph::vertex_descriptor v;
+   boost::graph_traits<Graph>::vertex_iterator vit, vend;
+   std::set<std::size_t>::const_iterator found; 
+   
    read_triplet(filename,transit_as);
-
+   //Parcours du graph
+   for( boost::tie ( vit,vend)  = boost::vertices( graph ); vit != vend; ++vit)
+   {
+      found=transit_as.find(*vit);
+      v=*vit;
+      //Affectation de la propriété transit
+      graph[v].is_transit=(found == transit_as.end())?false:true; 
+   }
 }
 
--- boost/src/graph/file_reader.hpp
+++ boost/src/graph/file_reader.hpp
@@ -9,9 +9,7 @@
 #include "graph.hpp"
 #include "../exceptions/ReaderException.hpp"
 #include "../utils/logger.hpp"
-
-enum{P2C,C2P,PEER};
+enum {P2C,C2P,PEER};
 void parse(std::string const & filename, Graph &);
 void read_triplet(std::string const & input_file,std::set<std::size_t> &  transit_as);
-
 #endif
